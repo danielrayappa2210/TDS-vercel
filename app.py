6961e6f2d25d24dbae86a7126e3c38c19f19f77c
danielrayappa2210 <daniel.putta@gramener.org>
2025-03-13 00:37:01 +0530

    Update app.py
    Debug response error

--- app.py
+++ app.py
@@ -34,10 +34,11 @@ def api():
     except Exception as e:
         return jsonify({"error": f"Error: {e}"}), 500
 
+    response = jsonify({"marks": marks})
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
-    return jsonify({"marks": marks})
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
